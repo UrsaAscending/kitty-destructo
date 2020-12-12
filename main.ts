1b66d3756de96a9076f5bb20d6aa0d578f81262b
UrsaAscending <75863565+UrsaAscending@users.noreply.github.com>
2020-12-12 16:06:42 -0600

    Give each enemy kind different life and score.  Add boss squid.

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ namespace SpriteKind {
     export const Tree = SpriteKind.create()
     export const Witch = SpriteKind.create()
     export const Shark = SpriteKind.create()
+    export const Squid = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Witch, function (sprite, otherSprite) {
     info.changeScoreBy(-4)
@@ -16,87 +17,102 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Shark, function (sprite, otherSp
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
-        . . b b b b . . 
-        . b 5 3 5 5 b . 
-        b 5 d 3 3 d 5 b 
-        b 5 3 5 5 1 5 b 
-        c 5 3 5 5 1 d c 
-        c d d 1 1 d d c 
-        . f d d d d f . 
-        . . f f f f . . 
-        `, mySprite, 50, 0)
+        . . b b b b . .
+        . b 5 3 5 5 b .
+        b 5 d 3 3 d 5 b
+        b 5 3 5 5 1 5 b
+        c 5 3 5 5 1 d c
+        c d d 1 1 d d c
+        . f d d d d f .
+        . . f f f f . .
+    `, mySprite, 50, 0)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Robot, function (sprite, otherSprite) {
     info.changeScoreBy(-2)
     scene.cameraShake(2, 500)
     otherSprite.destroy()
 })
+function level1Boss () {
+    if (!(bossSpawned)) {
+        enemySprite = sprites.create(img`
+            ................................
+            ................................
+            ................................
+            ................................
+            .........2222222................
+            .........2222222................
+            .........2552552................
+            .........2552552................
+            .........2222222................
+            .........2222222................
+            ...........222..................
+            ...........222..................
+            ...........222..................
+            ...........222..................
+            ...........222..................
+            .........2222222................
+            ...2222222222222222222..........
+            ...2222222222222222222..........
+            ...2222222222222222222..........
+            ...22....2222222....22..........
+            .........2222222................
+            .....2222..222..2222............
+            .....2552..222..2552............
+            .....255222222222552............
+            .....222222222222222............
+            ...........222..................
+            ...........222..................
+            ......222..222..222.............
+            ......252..222..252.............
+            ......222..222..222.............
+            ......2222222222222.............
+            ......2222222222222.............
+        `, SpriteKind.Squid)
+        enemySprite.setPosition(128, 44)
+        sprites.setDataNumber(enemySprite, "life", 13)
+    }
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Tree, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeScoreBy(3)
-    sprite.startEffect(effects.trail)
+    sprite.destroy()
+    sprites.changeDataNumberBy(otherSprite, "life", -1)
+    if (sprites.readDataNumber(otherSprite, "life") == 0) {
+        otherSprite.destroy()
+        info.changeScoreBy(3)
+        sprite.startEffect(effects.trail)
+    } else {
+        otherSprite.x += 50
+    }
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Witch, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeScoreBy(4)
-    sprite.startEffect(effects.trail)
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Squid, function (sprite, otherSprite) {
+    sprite.destroy()
+    sprites.changeDataNumberBy(otherSprite, "life", -1)
+    if (sprites.readDataNumber(otherSprite, "life") == 0) {
+        otherSprite.destroy()
+        info.changeScoreBy(100)
+        sprite.startEffect(effects.trail)
+    }
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Robot, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeScoreBy(2)
-    sprite.startEffect(effects.trail)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Tree, function (sprite, otherSprite) {
-    info.changeScoreBy(-3)
-    scene.cameraShake(3, 500)
-    otherSprite.destroy()
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Shark, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeScoreBy(1)
-    sprite.startEffect(effects.trail)
-})
-let enemySprite: Sprite = null
-let projectile: Sprite = null
-let mySprite: Sprite = null
-scene.setBackgroundColor(2)
-mySprite = sprites.create(img`
-    e e e . . . . e e e . . . . 
-    c d d c . . c d d c . . . . 
-    c b d d f f d d b c . . . . 
-    c 3 b d d b d b 3 c . . . . 
-    f b 3 d d d d 3 b f . . . . 
-    e d d d d d d d d e . . . . 
-    e d f d d d d f d e . b f b 
-    f d d f d d f d d f . f d f 
-    f b d d b b d d 2 f . f d f 
-    . f 2 2 2 2 2 2 b b f f d f 
-    . f b d d d d d d b b d b f 
-    . f d d d d d b d d f f f . 
-    . f d f f f d f f d f . . . 
-    . f f . . f f . . f f . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-game.onUpdateInterval(500, function () {
+function level1Enemies () {
     if (Math.percentChance(25)) {
         enemySprite = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . . 9 9 9 9 9 9 . . . . . . . 
-            . . . 5 9 9 9 9 5 . . . . . . . 
-            . . . . . 9 9 . . . . . . . . . 
-            . . . . e e e e . . . . . . . . 
-            . . . . e 5 5 e . . . . . . . . 
-            . . 9 9 e 5 5 e 9 9 . . . . . . 
-            . . 9 . e e e e . 9 . . . . . . 
-            . . 5 . 3 3 3 3 . 5 . . . . . . 
-            . . . . 3 3 3 3 . . . . . . . . 
-            . . 3 3 3 3 3 3 3 3 . . . . . . 
-            . . 3 . . . . . . 3 . . . . . . 
-            . 3 3 3 . . . . 3 3 3 . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Robot)
+            . . . . . . . . . . . . . . . .
+            . . . 9 9 9 9 9 9 . . . . . . .
+            . . . 5 9 9 9 9 5 . . . . . . .
+            . . . . . 9 9 . . . . . . . . .
+            . . . . e e e e . . . . . . . .
+            . . . . e 5 5 e . . . . . . . .
+            . . 9 9 e 5 5 e 9 9 . . . . . .
+            . . 9 . e e e e . 9 . . . . . .
+            . . 5 . 3 3 3 3 . 5 . . . . . .
+            . . . . 3 3 3 3 . . . . . . . .
+            . . 3 3 3 3 3 3 3 3 . . . . . .
+            . . 3 . . . . . . 3 . . . . . .
+            . 3 3 3 . . . . 3 3 3 . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+            . . . . . . . . . . . . . . . .
+        `, SpriteKind.Robot)
+        sprites.setDataNumber(enemySprite, "life", 2)
     } else if (Math.percentChance(25)) {
         enemySprite = sprites.create(img`
             . . . 6 6 6 6 6 6 6 6 6 6 6 . . 
@@ -116,6 +132,7 @@ game.onUpdateInterval(500, function () {
             . . . . . . e e e e . . . . . . 
             . . . . . . e e e e . . . . . . 
             `, SpriteKind.Tree)
+        sprites.setDataNumber(enemySprite, "life", 3)
     } else if (Math.percentChance(25)) {
         enemySprite = sprites.create(img`
             ........................
@@ -143,6 +160,7 @@ game.onUpdateInterval(500, function () {
             ........................
             ........................
             `, SpriteKind.Witch)
+        sprites.setDataNumber(enemySprite, "life", 4)
     } else {
         enemySprite = sprites.create(img`
             ....................................
@@ -162,8 +180,82 @@ game.onUpdateInterval(500, function () {
             ........ccccccfbdbbbfcc..........fff
             ...............ffffff...............
             `, SpriteKind.Shark)
+        sprites.setDataNumber(enemySprite, "life", 1)
     }
     enemySprite.setPosition(randint(160, 170), randint(0, 120))
     enemySprite.setVelocity(-50, 0)
     enemySprite.setFlag(SpriteFlag.AutoDestroy, true)
+    bossSpawned = false
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Witch, function (sprite, otherSprite) {
+    sprite.destroy()
+    sprites.changeDataNumberBy(otherSprite, "life", -1)
+    if (sprites.readDataNumber(otherSprite, "life") == 0) {
+        otherSprite.destroy()
+        info.changeScoreBy(4)
+        sprite.startEffect(effects.trail)
+    } else {
+        otherSprite.x += 50
+    }
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Robot, function (sprite, otherSprite) {
+    sprite.destroy()
+    sprites.changeDataNumberBy(otherSprite, "life", -1)
+    if (sprites.readDataNumber(otherSprite, "life") == 0) {
+        otherSprite.destroy()
+        info.changeScoreBy(2)
+        sprite.startEffect(effects.trail)
+    } else {
+        otherSprite.x += 50
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Tree, function (sprite, otherSprite) {
+    info.changeScoreBy(-3)
+    scene.cameraShake(3, 500)
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Shark, function (sprite, otherSprite) {
+    sprite.destroy()
+    sprites.changeDataNumberBy(otherSprite, "life", -1)
+    if (sprites.readDataNumber(otherSprite, "life") == 0) {
+        otherSprite.destroy()
+        info.changeScoreBy(1)
+        sprite.startEffect(effects.trail)
+    } else {
+        otherSprite.x += 50
+    }
+})
+let levelHighScore = 0
+let enemySprite: Sprite = null
+let bossSpawned = false
+let projectile: Sprite = null
+let mySprite: Sprite = null
+let level = 0
+scene.setBackgroundColor(1)
+mySprite = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+game.onUpdateInterval(500, function () {
+    if (levelHighScore < info.score()) {
+        levelHighScore = info.score()
+    }
+    if (levelHighScore < 50) {
+        level1Enemies()
+    } else {
+        level1Boss()
+    }
 })
